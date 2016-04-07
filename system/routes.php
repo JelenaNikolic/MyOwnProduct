80f3698852643858225a9a62400b3ce00f30ba3b
JelenaNikolic <jelena@virtualnigrad.com>
2016-04-07 14:46:51 +0200

    Izmenjeno, valjda je to to

--- system/routes.php
+++ system/routes.php
@@ -11,8 +11,8 @@ class Routes {
 	
 	public function index($urls, $url)
 	{
-		if(in_array($url, $urls)){ //ukoliko se string nalazi u url-u
-			echo array_search($url, $urls); // na kojem mestu u okviru niza url-a se on nalazi
+		if(in_array($url, $urls)){ //da li mi ovo uopste treba ili ne?
+			echo array_search($url, $urls); // na kojem mestu u okviru niza url-ova se nalazi trazeni string (url)
 		}else{
 			echo "Error 404<br />";
 		}
